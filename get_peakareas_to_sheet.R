0b04b12ea6576df442ac5d456474d8ae0bc40bf2
niekwit <41302859+niekwit@users.noreply.github.com>
2022-03-10 09:40:37 +0000

    checks for results sheet

--- get_peakareas_to_sheet.R
+++ get_peakareas_to_sheet.R
@@ -23,34 +23,42 @@ if (length(file.name) == 0){
 sheets <- excel_sheets(file.name)
 sheets <- sheets[! sheets %in% c("Component", "mdlCalcs")]
 
-#create list to store sheets as data frames
-df.list <- vector(mode = "list", length = length(sheets))
-
-#get number of samples
-df <- read_excel(file.name, sheet = sheets[1], skip = 4)
-df <- df[!grepl("blank", df$`Sample ID`, ignore.case = TRUE), ]
-df <- df[complete.cases(df$`Sample ID`), ]
-nrows <- nrow(df)
-
-#create output data frame
-df.out <- data.frame(matrix(ncol = length(sheets), nrow = nrows))
-names(df.out) <- sheets
-
-#read all data and store to data frame
-for (i in 1:length(sheets)){
-  #read sheet
-  df <- read_excel(file.name, sheet = sheets[i], skip = 4)
-  #select only sample sheets
+#check if results sheet is already present
+if ("Results" %in% sheets){
+  print("Results sheet already present in data file")
+} else {
+  #create list to store sheets as data frames
+  df.list <- vector(mode = "list", length = length(sheets))
+  
+  #get number of samples
+  df <- read_excel(file.name, sheet = sheets[1], skip = 4)
   df <- df[!grepl("blank", df$`Sample ID`, ignore.case = TRUE), ]
-  #remove rows with NA in Sample ID
   df <- df[complete.cases(df$`Sample ID`), ]
-  df.out[i] <- df$Area
+  nrows <- nrow(df)
+  
+  #create output data frame
+  df.out <- data.frame(matrix(ncol = length(sheets), nrow = nrows))
+  names(df.out) <- sheets
+  
+  #read all data and store to data frame
+  for (i in 1:length(sheets)){
+    #read sheet
+    df <- read_excel(file.name, sheet = sheets[i], skip = 4)
+    #select only sample sheets
+    df <- df[!grepl("blank", df$`Sample ID`, ignore.case = TRUE), ]
+    #remove rows with NA in Sample ID
+    df <- df[complete.cases(df$`Sample ID`), ]
+    df.out[i] <- df$Area
+  }
+  
+  #add sample id column
+  df.out <-cbind(`Sample ID` = df$`Sample ID`, df.out)
+  
+  #append results data to original file
+  write.xlsx(df.out, file.name, sheetName = "Results", append = TRUE, row.names = FALSE)
+  
 }
 
-#add sample id column
-df.out <-cbind(`Sample ID` = df$`Sample ID`, df.out)
 
-#append results data to original file
-write.xlsx(df.out, file.name, sheetName="Results", append=TRUE, row.names = FALSE)
 
 
