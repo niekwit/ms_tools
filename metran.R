6e72b41de6d6e76dea245118885a0b0df10a717d
niekwit <41302859+niekwit@users.noreply.github.com>
2023-01-10 13:10:51 +0000

    fixed bugs

--- metran.R
+++ metran.R
@@ -2,16 +2,20 @@
 #data is in triplicate
 #first sample is reference sample
 
-###First make sure cell counts are entered at the bottom of the file (first column: cell_counts)###
+###First make sure cell counts are entered at the bottom of the Data(corrected).csv file (first column: cell_counts)###
 
 #script directory
 script.dir <- "/home/niek/Documents/scripts/ms_tools"
 
 #set data directory
-setwd("/run/user/1000/gvfs/smb-share:server=jcbc-store01.jcbc.private.cam.ac.uk,share=citiid_home/nw416/My Documents/Data/HDAC3 project/Results/GC-MS/2022_09_26 Niek minusHT2/AIAEXPRT.AIA")
+work.dir <- "/home/niek/Desktop/2022_T-cells/AIAEXPRT.AIA"
+setwd(work.dir)
 
 #prepare data for read into R
-system(paste("bash",file.path(script.dir,"prepare4metran.sh")))
+system(paste("bash",file.path(script.dir,"prepare4metran.sh"),work.dir))
+
+#
+setwd(work.dir)
 
 #isotopomers to use as internal control for each fragment
 
@@ -29,75 +33,90 @@ control.list <- list("Pyr_"="M3",
                      "Glu_"="M5",
                      "Gln_"="M4",
                      "Asp_"="M4",
-                     "Ala_"="M3",
-                     "Cys_"="M3")
+                     "Ala_"="M3")
 
 #read corrected data
 df <- read.csv("data_corrected_4processing.csv", header = TRUE)
 
-#tidy up data frame
-df <- df[-grep("293[tT]",colnames(df))] #removes all columns with 293T controls
-
-#perform calculations for each fragment
-for (i in names(control.list)){
-  #subset for fragment
-  df.temp <- df[grepl(i, df$Ion),]
-  rownames(df.temp) <- NULL
-  
-  #get internal control m/z
-  control_ion <- control.list[[i]]
-  
-  #correct data for internal control
-  df.corrected <- df.temp[grepl("M0", df.temp$Ion), ]
-  df.corrected[2,] <- df.temp[grepl(control_ion, df.temp$Ion), ]
-  df.corrected[3, 1] <- paste0("M0/",control_ion)
-  df.corrected[3, 2:ncol(df.corrected)] <- df.corrected[1, 2:ncol(df.corrected)] / df.corrected[2, 2:ncol(df.corrected)]
-  rownames(df.corrected) <- NULL
-  
-  #add cell counts
-  df.corrected[4,] <- df[nrow(df),]
-  rownames(df.corrected) <- NULL
-  
-  #correct for cell counts
-  df.corrected[5,1] <- "corrected_for_cell_counts"
-  df.corrected[5,2:ncol(df.corrected)] <- df.corrected[3,2:ncol(df.corrected)] / df.corrected[4,2:ncol(df.corrected)]
-  
-  #calculate mean of triplicates
-  total.sample.number <- ncol(df.corrected) - 1
-  sample.number <- total.sample.number / 3
-  range.list <- vector("list",length=sample.number)
-  for (j in 1:sample.number){ #determine column numbers to be grouped
-    range.list[[j]] <- seq(((j*3)-1),((j*3)+1),1)
-  }
-  df.corrected[6,1] <- "mean_triplicates"
-  for (r in range.list){
-    df.corrected[6, r] <- mean(unlist(df.corrected[5, r]))
-  }
-  
-  #sample relative to control
-  df.corrected[7,1] <- "Relative_to_control"
-  for (r in range.list){
-    df.corrected[7, r] <- df.corrected[6, r] / df.corrected[6, range.list[[1]]]
-  }
-  
-  #add spacer
-  df.corrected[8,] <- NA
-  
-  #create data frame to write calculations to
-  if (exists("df.out") == FALSE){
-    df.out <- df.corrected
-  } else{
-    df.out <- rbind(df.out, df.corrected)
-  }
-  
+#remove any 293T data columns if present
+if (ncol(df[-grep("293[tT]",colnames(df))]) > 0){
+  df <- df[-grep("293[tT]",colnames(df))] #removes all columns with 293T controls
 }
 
-#write to file
-write.csv(df.out, 
-          file="data_corrected_analysed.csv", 
-          quote=FALSE,
-          na = ",",
-          row.names=FALSE)
+#remove theory column
+df$Theory <- NULL
+
+#check if data is in triplicate
+if ((ncol(df)-1)%%3 !=0){
+  print("ERROR: data not in triplicate")
+} else{
+  #do calculations if cell counts are entered
+  if (nrow(df[grep("cell_counts",df$Ion), ]) == 1){
+    #perform calculations for each fragment
+    for (i in names(control.list)){
+      #subset for fragment
+      df.temp <- df[grepl(i, df$Ion),]
+      rownames(df.temp) <- NULL
+      
+      #get internal control m/z
+      control_ion <- control.list[[i]]
+      
+      #correct data for internal control
+      df.corrected <- df.temp[grepl("M0", df.temp$Ion), ]
+      df.corrected[2,] <- df.temp[grepl(control_ion, df.temp$Ion), ]
+      df.corrected[3, 1] <- paste0("M0/",control_ion)
+      df.corrected[3, 2:ncol(df.corrected)] <- df.corrected[1, 2:ncol(df.corrected)] / df.corrected[2, 2:ncol(df.corrected)]
+      rownames(df.corrected) <- NULL
+      
+      #add cell counts
+      df.corrected[4,] <- df[nrow(df),]
+      rownames(df.corrected) <- NULL
+      
+      #correct for cell counts
+      df.corrected[5,1] <- "corrected_for_cell_counts"
+      df.corrected[5,2:ncol(df.corrected)] <- df.corrected[3,2:ncol(df.corrected)] / df.corrected[4,2:ncol(df.corrected)]
+      
+      #calculate mean of triplicates
+      total.sample.number <- ncol(df.corrected) - 1
+      sample.number <- total.sample.number / 3
+      range.list <- vector("list",length=sample.number)
+      for (j in 1:sample.number){ #determine column numbers to be grouped
+        range.list[[j]] <- seq(((j*3)-1),((j*3)+1),1)
+      }
+      df.corrected[6,1] <- "mean_triplicates"
+      for (r in range.list){
+        df.corrected[6, r] <- mean(unlist(df.corrected[5, r]))
+      }
+      
+      #sample relative to control
+      df.corrected[7,1] <- "Relative_to_control"
+      for (r in range.list){
+        df.corrected[7, r] <- df.corrected[6, r] / df.corrected[6, range.list[[1]]]
+      }
+      
+      #add spacer
+      df.corrected[8,] <- NA
+      
+      #create data frame to write calculations to
+      if (exists("df.out") == FALSE){
+        df.out <- df.corrected
+      } else{
+        df.out <- rbind(df.out, df.corrected)
+      }
+      
+    }
+    
+    #write to file
+    write.csv(df.out, 
+              file="data_corrected_analysed.csv", 
+              quote=FALSE,
+              na = ",",
+              row.names=FALSE)
+  } else {
+    print("ERROR: multiple or no 'cell_count' rows found.")
+  }
+}
+
 
 
 
--- prepare4metran.sh
+++ prepare4metran.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-#sometimes Matlab does not output all field delimters when there is no data (,)
+#get parsed arguments
+WORK_DIR=$1
+
+#go to data dir
+cd $WORK_DIR
+
+#sometimes Matlab does not output all field delimiters when there is no data (,)
 FIRST_LINE=$(head -1 'Data(corrected).csv' | grep ',')
 
 if [[ -z $FIRST_LINE ]]
@@ -18,13 +24,15 @@ else
 	LINE_NUMBER=$(grep -n 'Mass isotopomer abundances' 'Data(corrected).csv' | awk -F ':' '{print $1}')
 
 	#tidy up file
-	END=$(head -1 'Data(corrected).csv' | awk -F, '{print NF-2}')
-	cut -d, -f1-$END 'Data(corrected).csv' > data_corrected_4processing_temp.csv
+	#END=$(head -1 'Data(corrected).csv' | awk -F, '{print NF-2}')
+	#cut -d, -f1-$END 'Data(corrected).csv' > data_corrected_4processing_temp.csv
 
 	#output file with just mass isotopomer data
 	sed "1,${LINE_NUMBER}d" 'data_corrected_4processing_temp.csv' | grep -v '^,*$' > data_corrected_4processing.csv
 	rm 'data_corrected_4processing_temp.csv'
 fi
+
+
 
 
 
